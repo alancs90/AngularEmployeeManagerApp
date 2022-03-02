88168da825c815a108c064f40cac178daa27eb76
Alan Carrasco <alancs90@gmail.com>
2022-03-01 21:39:41 -0500

    auto-update 2022-03-01-21:39:41

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   // title = 'employeemanagerapp';
 
   public employees: Employee[] = [];
-  
+
   constructor(private EmployeeService: EmployeeService){}
 
   ngOnInit(){
@@ -28,5 +28,26 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+
+    container.appendChild(button);
+    button.click();
   }
 }
